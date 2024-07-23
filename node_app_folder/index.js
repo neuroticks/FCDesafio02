bed4a2fabcc7af4398251483ce849fb545af19e3
Rodrigo Ferraro <neuroticks@gmail.com>
2024-07-23 11:45:37 -0300

    agora a porta vem do .env

--- node_app_folder/index.js
+++ node_app_folder/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
     res.send('<h1> Full Cycle Rocks!</h1>')
 })
 
-app.listen(port, () => {
-    console.log(`Rodando na porta ${port}`);
+app.listen(process.env.NODE_APP_SERVER_PORT, () => {
+    console.log(`Rodando na porta ${process.env.NODE_APP_SERVER_PORT}`);
 });
