2a440abefbbaae5506ef4365baad2a0807e91793
Rodrigo Ferraro <neuroticks@gmail.com>
2024-07-24 18:49:56 -0300

    inclusão do nodemon. agora toda atualização em arquivos do projeto causa o reload automatico do container - atualizado.

--- node_app_folder/index.js
+++ node_app_folder/index.js
@@ -10,13 +10,10 @@ const db_config = {
 
 async function insert_and_get_nome() {
     try {
-        //console.log(`==== user = ${db_config.user} +++++++++`);
-        //console.log(`==== password = ${db_config.password} +++++++++`);
-
         const connection = await mysql.createConnection(db_config);
         console.log('Conectado ao banco');
 
-        const nome = 'Rodrigo';
+        const nome = 'John Doe';
         const insertQuery = 'INSERT INTO pessoa (nome) VALUES (?)';
         await connection.execute(insertQuery, [nome]);
         console.log(`Nome inserido >> ${nome}`);
